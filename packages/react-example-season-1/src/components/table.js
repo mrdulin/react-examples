995ddca4a8070146bc1e63e4d0c1fa9bdbc044e0
mrdulin <novaline.dulin@gmail.com>
2016-05-25 02:41:54 +0000

    c9

---
+++ packages/react-example-season-1/src/components/table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Table extends React.Component{
+    
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>hello</div>;
+    }
+}
+
+export default Table;
---
+++ packages/react-example-season-1/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+
+import Table from './components/table';
+
+
+ReactDOM.render(
+    <Table/>,
+    document.getElementById('container')
+);
