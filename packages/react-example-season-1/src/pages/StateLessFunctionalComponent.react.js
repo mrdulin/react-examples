ca1f9d4b93278911c79f136e3952ee6067872037
mrdulin <novaline.dulin@gmail.com>
2016-06-30 12:27:41 +0800

    nothing change

--- packages/react-example-season-1/src/pages/StateLessFunctionalComponent.react.js
+++ packages/react-example-season-1/src/pages/StateLessFunctionalComponent.react.js
@@ -1,45 +1,47 @@
 import React, {Component, PropTypes} from 'react';
 
 const Text = ({children}) => {
-    return (
-        <p>{children}</p>
-    );
+	return (
+		<p>{children}</p>
+	);
 };
 
-Text.defaultProps = {children: 'I\'m a stateless functional component'}
+Text.defaultProps = {
+	children: 'I\'m a stateless functional component'
+}
 Text.propTypes = {
-    children: PropTypes.string.isRequired
+	children: PropTypes.string.isRequired
 };
 
 const Book = (props, context) => {
-    console.log('Book Context', context);
-    console.log('Book props', props);
-    return (
-        <p style={context}>{props.children}</p>
-    );
+	console.log('Book Context', context);
+	console.log('Book props', props);
+	return (
+		<p style={context}>{props.children}</p>
+	);
 }
 
 Book.contextTypes = {
-    color: PropTypes.string
+	color: PropTypes.string
 }
 
-class StateLessFunctionalComponent extends Component{
-    static childContextTypes = {
-        color: PropTypes.string
-    }
+class StateLessFunctionalComponent extends Component {
+	static childContextTypes = {
+		color: PropTypes.string
+	}
 
-    getChildContext() {
-        return {color: 'purple'};
-    }
+	getChildContext() {
+		return {color: 'purple'};
+	}
 
-    render() {
-        return (
-            <div>
-                <Text></Text>
-                <Book>Book name is 《React by Examples》</Book>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Text></Text>
+				<Book>Book name is 《React by Examples》</Book>
+			</div>
+		);
+	}
 }
 
 export default StateLessFunctionalComponent;
