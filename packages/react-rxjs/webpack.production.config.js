adf79e444735e123a1be616df06cf589964f0139
mrdulin <novaline.dulin@gmail.com>
2016-06-30 20:18:55 +0800

    improve webpack config

--- packages/react-rxjs/webpack.production.config.js
+++ packages/react-rxjs/webpack.production.config.js
@@ -4,22 +4,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
+const src = __dirname + '/src/';
+const dist = __dirname + '/dist/';
+
 module.exports = {
-    entry: __dirname + '/src/app.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.[hash].js'
+    entry: {
+        app: src + 'app.js',
+        lib: src + 'lib.js'
     },
+    output: {
+        path: dist,
+        filename: '[name].[hash].js'
+    },
+    devtool: "source-map",
     module: {
-        loaders: [
-            {test: /\.js$/, exclude: /(node_modules|bower_components)/, loaders: ['react-hot', 'babel']},
-            {test: /\.json$/, loaders: ['json']},
-            {test: /\.scss$/, exclude: /(node_modules|bower_components)/, loader: ExtractTextPlugin.extract(
-                    'style', // The backup style loader
-                    'css?sourceMap!postcss!sass?sourceMap'
-                )
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loaders: ['react-hot', 'babel']
+        }, {
+            test: /\.json$/,
+            loaders: ['json']
+        }, {
+            test: /\.scss$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: ExtractTextPlugin.extract(
+                'style', // The backup style loader
+                'css?sourceMap!postcss!sass?sourceMap'
+            )
+        }, {
+            test: /\.(png|jpg|jpeg|gif|woff)$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'url-loader',
+            query: {
+                limit: 8192
             }
-        ]
+        }]
     },
     //解决import scss文件时相对路径的问题
     sassLoader: {
@@ -36,12 +56,17 @@ module.exports = {
             verbose: true,
             dry: false
         }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.[hash].js'
+        }),
         new HtmlWebpackPlugin({
-            title: 'React Weather',
+            template: src + 'index.html',
+            filename: 'index.html',
             hash: true
         }),
         new ExtractTextPlugin('style.[hash].css', {
-          allChunks: true
+            allChunks: true
         }),
         new webpack.BannerPlugin("Copyright Novaline"),
         new webpack.optimize.UglifyJsPlugin({
