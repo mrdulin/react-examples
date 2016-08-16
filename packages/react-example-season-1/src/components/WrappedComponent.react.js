d5a1275bad296e234e4bc29c27884a5795c67ed7
mrdulin <novaline.dulin@gmail.com>
2016-08-16 14:10:37 +0800

    update HOC

--- packages/react-example-season-1/src/components/WrappedComponent.react.js
+++ packages/react-example-season-1/src/components/WrappedComponent.react.js
@@ -1,19 +1,53 @@
 import React, {Component} from 'react';
 
-class WrappedComponent extends Component{
-    render() {
-        return (
-            <div>WrappedComponent</div>
-        );
-    }
-}
-
 function ppHOC(WrappedComponent) {
     return class extends Component{
+        state = {
+            keyword: ''
+        }
+        constructor() {
+            super();
+            this.searchChange = ::this.searchChange;
+            this.proc = ::this.proc;
+        }
+
+        proc (WrappedComponentInstance) {
+            //使用ref取得组件实例的引用
+            console.log('WrappedComponentInstance', WrappedComponentInstance);
+            WrappedComponentInstance.method();
+        }
+
+        searchChange(e) {
+            this.setState({keyword: e.target.value});
+        }
         render() {
-            return <WrappedComponent {...this.props}/>
+            const props = Object.assign({}, this.props, {ref: this.proc}, {
+                search: {
+                    value: this.state.keyword,
+                    onChange: this.searchChange
+                }
+            });
+
+            return <WrappedComponent {...props}/>
         }
     }
 }
+
+class WrappedComponent extends Component{
+    render() {
+        const {search} = this.props;
+        return (
+            <div>
+                <input type="text" {...search}/>
+                <p>{search.value}</p>
+            </div>
+        );
+    }
+
+    method() {
+        console.log('WrappedComponent method executed');
+    }
+}
+
 
 export default ppHOC(WrappedComponent);
