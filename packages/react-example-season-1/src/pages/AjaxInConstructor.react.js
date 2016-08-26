600a4a60c0b515387dbcd3a3c9c0b74135f39cf5
mrdulin <novaline.dulin@gmail.com>
2016-08-26 16:25:56 +0800

    update demo

--- packages/react-example-season-1/src/pages/AjaxInConstructor.react.js
+++ packages/react-example-season-1/src/pages/AjaxInConstructor.react.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class AjaxInConstructor extends Component{
     constructor() {
         super();
-        this.state = {name: '', age: ''}
+        this.state = {name: '', age: '', content: ''}
         const loadDataSuccess = data => {
             this.setState(data);
         };
@@ -17,14 +17,37 @@ class AjaxInConstructor extends Component{
                     age: 123
                 });
             }, 2000);
-        });
+        }).then((person) => {
+            return this.loadBooks().then(data => {
+                return Object.assign({}, person, {content: data});
+            });
+        })
     }
+
+    loadBooks() {
+        const url = 'http://it-ebooks-api.info/v1/search/angular';
+        return fetch(url).then(res => res.json());
+    }
+
     render() {
-        const {name, age} = this.state;
+        const {name, age, content} = this.state;
+        const {Books = []} = content;
+
+        const bookItems = Books.map(book => {
+            return <li key={book.ID}>
+                <h1>{book.Title}</h1>
+                <p>{book.Description}</p>
+                <img src={book.Image} alt=''/>  
+            </li>
+        })
+
         return <div>
             <p>Can I init component state async?</p>
             <p>name: {name}</p>
             <p>age: {age}</p>
+            <ul>
+            {bookItems}
+            </ul>
         </div>
     }
 }
--- packages/react-example-season-1/src/pages/FunctionBind.react.js
+++ packages/react-example-season-1/src/pages/FunctionBind.react.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 
 class FunctionBind extends Component{
+
+    constructor() {
+        super();
+        this.method4 = this.method4.bind(this);
+    }
     componentDidMount() {
         const {filter, map} = Array.prototype;
         var el = document.querySelectorAll('p')::filter(el => {
             return el.id !== 'fucking-SB';
         })::map(p => p.style.cssText = 'background-color: blue; color: #fff');
     }
+
+    method1 = () => {
+        console.log('method1', this);   //'method1' FunctionBind
+    }
+
+    method2() {
+        console.log('method2', this);  //'method2' null
+    }
+
+    method3() {
+        console.log('method3', this);   //'method3' FunctionBind
+    }
+
+    method4() {
+        console.log('method4', this);   //'method4' FunctionBind
+    }
+
     render() {
         return <div className='fn-bind-container'>
             <p id='fucking-SB'></p>
             <p>fucking SB hard</p>
             <p>If you don't know, shut your fucking mouth up!</p>
+            <button onClick={this.method1}>call method1</button>
+            <button onClick={this.method2}>call method2</button>
+            <button onClick={() => this.method3()}>call method3</button>
+            <button onClick={this.method4}>call method4</button>
         </div>
     }
 }
