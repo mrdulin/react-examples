bc69c5f3e9cc1f88923ba389cfb33c8afb16c8d7
mrdulin <novaline.dulin@gmail.com>
2016-08-26 11:47:43 +0800

    add a demo

--- packages/react-example-season-1/src/pages/setChildrenReactElementStyle.react.jsx
+++ packages/react-example-season-1/src/pages/setChildrenReactElementStyle.react.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
 class StyleByParentComponent extends Component{
+    static defaultProps = {
+        cycle: {borderRadius: '50%', width: '100px', height: '100px', backgroundColor: 'red'},
+        square: {width: '100px', height: '100px', backgroundColor: 'lightblue'}
+    }
+
     render() {
         const children = React.Children.map(this.props.children, (child, idx) => {
-            console.log(child);
-            return React.cloneElement(child, {style: {borderRadius: '50%', width: '100px', height: '100px', backgroundColor: 'red'}});
+            console.dir(child);
+            const className = child.props.className;
+            return React.cloneElement(child, {style: this.props[className]});
         })
         return <div>
             child component
