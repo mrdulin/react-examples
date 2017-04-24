863a4eeb4c3de5080a70a9519545d6ac80ec5c44
mrdulin <novaline.dulin@gmail.com>
2017-04-24 14:13:07 +0800

    添加一个例子

--- packages/react-example-season-2/app/index.js
+++ packages/react-example-season-2/app/index.js
@@ -1,8 +1,9 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 import store from 'app/store';
+import { render, unmountComponentAtNode } from 'react-dom';
 
-store.dispatch({type: 'globalAction'});
+store.dispatch({ type: 'globalAction' });
 
 import Perf from 'react-addons-Perf';
 window.Perf = Perf;
@@ -10,7 +11,22 @@ window.Perf = Perf;
 import 'common/scss';
 import Root from './routes';
 
-ReactDOM.render(
-    <Root />,
-    document.getElementById('container')
-)
+const dom_container = document.getElementById('container');
+
+let instance = render(<Root />, dom_container);
+
+window.renderComponent = () => {
+  console.log('挂载');
+  instance = ReactDOM.render(<Root />, dom_container);
+};
+
+window.setState = () => {
+  console.log('更新');
+  instance.setState({ foo: 'barbar' });
+};
+
+window.unmountComponentAtNode = () => {
+  console.log('卸载');
+  unmountComponentAtNode(dom_container);
+};
+
--- packages/react-example-season-2/app/routes.js
+++ packages/react-example-season-2/app/routes.js
@@ -6,54 +6,84 @@ import createHashHistory from 'history/lib/createHashHistory';
 import store from './store';
 
 const routes = {
-    path: '/',
-    component: require('modules/Home'),
-    getChildRoutes(nextState, cb) {
-        require.ensure([], require => {
-            cb(null, [
-                require('./modules/Ip'),
-                require('./modules/Mobile'),
-                require('./modules/Toutiao'),
-                require('./modules/transitionToSelf'),
-                require('./modules/CustomAttributes'),
-                require('./modules/DidMountError'),
-                require('./modules/EventArguments'),
-                require('./modules/Animation'),
-                require('./modules/getWrappedInstance'),
-                require('./modules/SCU'),
-                require('./modules/RouterHOC'),
-                require('./modules/CallComponentMethodOutside'),
-                require('./modules/CoreJSAndTransformRuntime'),
-                require('./modules/ImageZoom'),
-                require('./modules/ReactPinchZoom'),
-                require('./modules/SearchBook'),
-                require('./modules/TestScroller'),
-                require('./modules/TestStarRating'),
-                require('./modules/ImageUpload'),
-                require('./modules/ImmutabilityHelper'),
-                require('./modules/AvoidRequireDuplication'),
-                require('./modules/DataRef'),
-                require('./modules/ReturnListMapDirectly'),
-                require('./modules/FunctionAsChildComponents'),
-                require('./modules/DefaultProps'),
-                {
-                    path: '*',
-                    onEnter: (nState, replace) => replace('/')
-                }
-            ]);
-        }, 'dynamicRoutes');
-    }
+  path: '/',
+  component: require('modules/Home'),
+  getChildRoutes(nextState, cb) {
+    require.ensure([], require => {
+      cb(null, [
+        require('./modules/Ip'),
+        require('./modules/Mobile'),
+        require('./modules/Toutiao'),
+        require('./modules/transitionToSelf'),
+        require('./modules/CustomAttributes'),
+        require('./modules/DidMountError'),
+        require('./modules/EventArguments'),
+        require('./modules/Animation'),
+        require('./modules/getWrappedInstance'),
+        require('./modules/SCU'),
+        require('./modules/RouterHOC'),
+        require('./modules/CallComponentMethodOutside'),
+        require('./modules/CoreJSAndTransformRuntime'),
+        require('./modules/ImageZoom'),
+        require('./modules/ReactPinchZoom'),
+        require('./modules/SearchBook'),
+        require('./modules/TestScroller'),
+        require('./modules/TestStarRating'),
+        require('./modules/ImageUpload'),
+        require('./modules/ImmutabilityHelper'),
+        require('./modules/AvoidRequireDuplication'),
+        require('./modules/DataRef'),
+        require('./modules/ReturnListMapDirectly'),
+        require('./modules/FunctionAsChildComponents'),
+        require('./modules/DefaultProps'),
+        {
+          path: '*',
+          onEnter: (nState, replace) => replace('/')
+        }
+      ]);
+    }, 'dynamicRoutes');
+  }
 };
 
-const history = useRouterHistory(createHashHistory)({queryKey: false});
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
 
-const Root = () => {
-    return <Provider store={store}>
-        <MuiThemeProvider>
+/*const Root = () => {
+  return <Provider store={store}>
+    <MuiThemeProvider>
+      <Router history={__PROD__ ? history : browserHistory} routes={routes} />
+    </MuiThemeProvider>
+  </Provider>
+};*/
+
+const suffix = '被调用, this指向： ';
+
+class Root extends React.Component {
+  state = {
+    foo: 'bar'
+  };
+
+  handler() {
+    console.log(`handler ${suffix}`, this);
+  }
+
+  render() {
+    const { foo } = this.state;
+    this.handler();
+    window.handler = this.handler;
+    window.handler();
+
+    return (
+      <div>
+        <p onClick={this.handler}>{foo}</p>
+        <Provider store={store}>
+          <MuiThemeProvider>
             <Router history={__PROD__ ? history : browserHistory} routes={routes} />
-        </MuiThemeProvider>
-    </Provider>
-};
+          </MuiThemeProvider>
+        </Provider>
+      </div>
+    )
+  }
+}
 
 export default Root;
 
