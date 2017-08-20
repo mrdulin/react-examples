87b6e197324d66c1b3f99ec49babdc57082199cc
mrdulin <novaline.dulin@gmail.com>
2017-08-20 14:05:18 +0800

    build

--- packages/react-ts-webpack/docs/scripts/manifest.js
+++ packages/react-ts-webpack/docs/scripts/manifest.js
@@ -88,7 +88,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "f73968b4612e129ca596"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "8a91ccbea7167b5b9dca"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -783,7 +783,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/react-ts-webpack2/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
