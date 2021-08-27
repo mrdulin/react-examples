0253a518ad6fd872e862e185ef7ca0b2f76f5c26
mrdulin <novaline.dulin@gmail.com>
2021-08-27 14:03:06 +0800

    feat: add prop-types typescript generic type example

---
+++ packages/v16/README.md
@@ -0,0 +1,1 @@
+# React V16 Examples
---
+++ packages/v16/examples/prop-types/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import * as PTs from "prop-types";
+
+interface Bill {
+  realtime: boolean;
+  premium: number;
+  nextPremium: number;
+  defaultSavingPremium: number;
+  defaultReducedPriceRate: number;
+}
+
+interface AppProps {
+  bill: Bill;
+}
+const App: React.FC<AppProps> = (props: AppProps) => {
+  return <div>app</div>;
+};
+
+App.propTypes = {
+  bill: PTs.exact<PTs.ValidationMap<Bill>>({
+    realtime: PTs.bool.isRequired,
+    premium: PTs.number.isRequired,
+    nextPremium: PTs.number.isRequired,
+    defaultSavingPremium: PTs.number.isRequired,
+    defaultReducedPriceRate: PTs.number.isRequired,
+  }),
+};
