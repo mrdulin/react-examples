3c9c38dd5f65cd854e2c18e98e882f9a6b5c3d64
mrdulin <novaline.dulin@gmail.com>
2023-06-28 16:40:38 +0800

    feat: add typed-scss-modules example

---
+++ v16/stackoverflow/76571086/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	parser: 'postcss-scss',
+	plugins: {
+		'postcss-modules': {
+			localsConvention: 'camelCaseOnly',
+		},
+	},
+};
---
+++ v16/stackoverflow/76571086/style.module.scss.d.ts
@@ -0,0 +1,9 @@
+export type Styles = {
+	'test-test': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
