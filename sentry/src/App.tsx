df87af75dd6262820cb6165c023a39f229039e39
mrdulin <novaline.dulin@gmail.com>
2023-08-08 15:56:46 +0800

    feat: remove useless component

--- sentry/src/App.tsx
+++ sentry/src/App.tsx
@@ -18,17 +18,6 @@ function App() {
 					className='App-logo'
 					alt='logo'
 				/>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
 				<Sentry.ErrorBoundary
 					fallback={<p>An error has occurred</p>}
 					showDialog
